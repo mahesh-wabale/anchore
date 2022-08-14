d07c298dde4db2c0c0316c62849b8a44e1304ce0
mahesh-wabale <maheshd7878@gmail.com>
2022-08-14 17:56:54 +0800

    Create helloword.c

---
+++ 14-aug-2022/helloword.c
@@ -0,0 +1,18 @@
+//Program name : Print msg on terminal 
+//Assignment : 1 
+//Date : 14-Aug-2022
+//Author : Mahesh D Wabale 
+
+//Expected input : 
+//Expected output :
+
+// include std input output file
+#include<stdio.h>
+
+// entry point function 
+int main() { 
+
+printf("My First c program"); // print msg on terminal 
+
+return 0; // returning response - successful
+} 
